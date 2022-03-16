35e8cc48eac72e0c87945563f852bbff2cf6343d
Valcelio Fontenle <vfonte2000@gmail.com>
2022-03-16 20:51:47 -0300

    CategoryResource

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,61 @@
+package com.devsuperior.dscatalog.entities;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private long id;
+	
+	private String nome;
+	
+	public Category () {
+		
+	}
+
+	public Category(long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	
+	
+}
