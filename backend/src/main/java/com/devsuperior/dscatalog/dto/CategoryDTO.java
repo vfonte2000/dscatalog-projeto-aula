1283e39d81a557d1cc04474a3571468e7bbe35a6
Valcelio Fontenle <vfonte2000@gmail.com>
2022-03-21 18:23:36 -0300

    add classes DTO

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
@@ -0,0 +1,43 @@
+package com.devsuperior.dscatalog.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dscatalog.entities.Category;
+
+public class CategoryDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String nome;
+
+	public CategoryDTO() {
+
+	}
+
+	public CategoryDTO(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public CategoryDTO(Category entity) {
+		this.id = entity.getId();
+		this.nome = entity.getNome();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryaResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryaResource.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+
 import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
@@ -19,8 +20,8 @@ public class CategoryaResource {
 	private CategoryService service; 
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
