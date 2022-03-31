bc4d5d7192d80df8f8116e1fc3d1c2d2883b5791
pietro dos santos fontenele <pietrofontenele2009@gmail.com>
2022-03-30 21:16:39 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,25 +1,36 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+
 @Entity
-@Table (name = "tb_category")
+@Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+
 	private String nome;
-	
-	public Category () {
-		
+
+	public Category() {
+
 	}
 
 	public Category(long id, String nome) {
@@ -43,6 +54,25 @@ public class Category implements Serializable {
 		this.nome = nome;
 	}
 
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdateAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	@PreUpdate
+	public void preUpdate () {
+		updatedAt = Instant.now();
+	}
+	
+	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -65,6 +95,4 @@ public class Category implements Serializable {
 		return true;
 	}
 
-	
-	
 }
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO TB_CATEGORY (nome) VALUES ('Books');
-INSERT INTO TB_CATEGORY (nome) VALUES ('Electronics');
-INSERT INTO TB_CATEGORY (nome) VALUES ('Computers');
+INSERT INTO TB_CATEGORY (nome, created_At) VALUES ('Books', NOW());
+INSERT INTO TB_CATEGORY (nome,created_At) VALUES ('Electronics', NOW());
+INSERT INTO TB_CATEGORY (nome,created_At) VALUES ('Computers', NOW());
